a5439fa95a3d57ef4c928bbedce71d190def6a39
UwU421 <87078266+UwU421@users.noreply.github.com>
2023-05-24 08:07:49 +0200

    Aktualizovat pxt.json, main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -15,7 +15,11 @@ def on_button_pressed_b():
     threshold += 5
 input.on_button_pressed(Button.B, on_button_pressed_b)
 
-radio.set_group(23)
+def on_received_value(name, value):
+    basic.show_string("Hello!")
+radio.on_received_value(on_received_value)
+
+radio.set_group(50)
 OLED.init(128, 64)
 threshold = 50
 points = [43, 86, 21, 42]
@@ -25,6 +29,15 @@ def on_forever():
     if smarthome.read_noise(AnalogPin.P3) > threshold:
         smarthome.motor_fan(AnalogPin.P9, True)
         OLED.write_string_new_line("that boy finna pullup in a hulk machine")
+        music.play_sound_effect(music.create_sound_effect(WaveShape.NOISE,
+                1110,
+                1110,
+                255,
+                255,
+                200,
+                SoundExpressionEffect.NONE,
+                InterpolationCurve.LINEAR),
+            SoundExpressionPlayMode.UNTIL_DONE)
     else:
         OLED.clear()
         smarthome.motor_fan(AnalogPin.P9, False)
--- main.ts
+++ main.ts
@@ -1,29 +1,47 @@
-input.onButtonPressed(Button.A, function on_button_pressed_a() {
-    
-    radio.sendString("hi")
+input.onButtonPressed(Button.A, function () {
     threshold += -5
 })
-radio.onReceivedString(function on_received_string(receivedString: string) {
-    music.playSoundEffect(music.builtinSoundEffect(soundExpression.giggle), SoundExpressionPlayMode.UntilDone)
-    basic.showString("Hello!")
-})
-input.onButtonPressed(Button.B, function on_button_pressed_b() {
-    
+function anal3 () {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sawtooth, 263, 1021, 255, 0, 500, SoundExpressionEffect.Tremolo, InterpolationCurve.Logarithmic), SoundExpressionPlayMode.UntilDone)
+}
+function playsound () {
+    temp = randint(0, sounds.length)
+    music.playSoundEffect(sounds[temp], SoundExpressionPlayMode.UntilDone)
+    basic.pause(100)
+}
+input.onButtonPressed(Button.B, function () {
     threshold += 5
 })
-radio.setGroup(23)
+function anal () {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Noise, 486, 129, 114, 255, 500, SoundExpressionEffect.Vibrato, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
+}
+function anal2 () {
+    music.playSoundEffect(music.createSoundEffect(WaveShape.Sawtooth, 486, 575, 70, 255, 500, SoundExpressionEffect.Tremolo, InterpolationCurve.Curve), SoundExpressionPlayMode.UntilDone)
+}
+let temp = 0
+let sounds: string[] = []
 OLED.init(128, 64)
 let threshold = 50
-let points = [43, 86, 21, 42]
-basic.showIcon(IconNames.Heart)
-basic.forever(function on_forever() {
+let strip = neopixel.create(DigitalPin.P12, 1, NeoPixelMode.RGBW)
+sounds = [
+music.createSoundEffect(WaveShape.Sawtooth, 464, 598, 255, 0, 500, SoundExpressionEffect.Tremolo, InterpolationCurve.Curve),
+music.createSoundEffect(WaveShape.Noise, 508, 63, 255, 0, 500, SoundExpressionEffect.Vibrato, InterpolationCurve.Curve),
+music.createSoundEffect(WaveShape.Sine, 200, 600, 255, 0, 150, SoundExpressionEffect.None, InterpolationCurve.Linear),
+music.createSoundEffect(WaveShape.Triangle, 152, 1891, 255, 255, 500, SoundExpressionEffect.Warble, InterpolationCurve.Logarithmic),
+music.createSoundEffect(WaveShape.Noise, 1, 1, 255, 255, 500, SoundExpressionEffect.Tremolo, InterpolationCurve.Curve),
+music.createSoundEffect(WaveShape.Square, 107, 107, 255, 255, 500, SoundExpressionEffect.Tremolo, InterpolationCurve.Logarithmic)
+]
+basic.showIcon(IconNames.Happy)
+basic.forever(function () {
     if (smarthome.ReadNoise(AnalogPin.P3) > threshold) {
+        strip.showColor(neopixel.colors(NeoPixelColors.Red))
         smarthome.motorFan(AnalogPin.P9, true)
         OLED.writeStringNewLine("that boy finna pullup in a hulk machine")
+        playsound()
     } else {
         OLED.clear()
+        strip.showColor(neopixel.colors(NeoPixelColors.Black))
         smarthome.motorFan(AnalogPin.P9, false)
     }
-    
     basic.pause(10)
 })
